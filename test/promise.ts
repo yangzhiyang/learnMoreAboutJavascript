d75f5d2a07974fb41b3c4a2a2309eb4e4081687f
冬霖 <zhiyang_yang@126.com>
2019-09-07 17:14:42 +0800

    :white_check_mark: feat(test/promise.ts): add a test case: Promise has to receive a function

--- test/promise.ts
+++ test/promise.ts
@@ -7,6 +7,11 @@ describe('Promise', () => {
   it('是一个类', () => {
     assert.isFunction(Promise);
     assert.isObject(Promise.prototype);
+  });
+  it('new Promise() 必须接收一个函数', () => {
+    assert.throw(() => {
+      new Promise();
+    })
   })
 })
 
