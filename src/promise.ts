54db42162d36c8dfefa578bc787bc00dd22324ea
冬霖 <zhiyang_yang@126.com>
2019-09-07 18:03:26 +0800

    :art(src/promise.ts): refactor: 优化代码

--- src/promise.ts
+++ src/promise.ts
@@ -1,19 +1,21 @@
 class MyPromise {
   succeed = null;
   fail = null;
+  resolve() {
+    setTimeout(() => this.succeed(), 0);
+  }
+  reject() {
+    setTimeout(() => this.fail(), 0);
+  }
   constructor(fn) {
-    if(typeof fn !== 'function') {
-      throw new Error('Promise只接收函数')
+    if (typeof fn !== "function") {
+      throw new Error("Promise只接收函数");
     }
-    fn(() => {
-      setTimeout(() => this.succeed(), 0);
-    }, () => {
-      setTimeout(() => this.fail(), 0);
-    });
+    fn(this.resolve.bind(this), this.reject.bind(this));
   }
   then(succeed, fail) {
     this.succeed = succeed;
-    this.fail = fail
+    this.fail = fail;
   }
 }
 
