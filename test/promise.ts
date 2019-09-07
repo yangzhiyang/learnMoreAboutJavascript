f0c7d95263f9ecc681414f1e271a59ab61dc1808
冬霖 <zhiyang_yang@126.com>
2019-09-07 19:11:12 +0800

    :hammer: refactor(test/promise.ts): 使用 sinon 测试函数

--- test/promise.ts
+++ test/promise.ts
@@ -1,14 +1,18 @@
-import * as chai from 'chai';
-import Promise from '../src/promise';
+import * as chai from "chai";
+import * as sinon from "sinon";
+import * as sinonChai from "sinon-chai";
+import Promise from "../src/promise";
+
+chai.use(sinonChai);
 
 const assert = chai.assert;
 
-describe('Promise', () => {
-  it('是一个类', () => {
+describe("Promise", () => {
+  it("是一个类", () => {
     assert.isFunction(Promise);
     assert.isObject(Promise.prototype);
   });
-  it('new Promise() 必须接收一个函数', () => {
+  it("new Promise() 必须接收一个函数", () => {
     assert.throw(() => {
       // @ts-ignore
       new Promise();
@@ -23,43 +27,36 @@ describe('Promise', () => {
     });
     assert.throw(() => {
       // @ts-ignore
-      new Promise('promise');
+      new Promise("promise");
     });
   });
-  it('new Promise(fn) 会生成一个带有 then 方法的对象', () => {
+  it("new Promise(fn) 会生成一个带有 then 方法的对象", () => {
     const promise = new Promise(() => {});
-    assert.isFunction(promise.then)
+    assert.isFunction(promise.then);
   });
-  it('new Promise(fn) 中传入的 fn 立即执行', () => {
-    let called = false;
-    const promise = new Promise(() => {
-      called = true;
-    });
-    assert.isTrue(called);
+  it("new Promise(fn) 中传入的 fn 立即执行", () => {
+    const fn = sinon.fake();
+    new Promise(fn);
+    assert(fn.called);
   });
-  it('fn 接收 resolve、reject 两个函数', () => {
-    let called = false;
-    const promise = new Promise((resolve, reject) => {
-      called = true;
+  it("fn 接收 resolve、reject 两个函数", done => {
+    new Promise((resolve, reject) => {
       assert.isFunction(resolve);
       assert.isFunction(reject);
+      done();
     });
-    assert.isTrue(called);
   });
-  it('promise.then(success) 中的 success 会在 reslove 被调用的时候执行', done => {
-    let called = false;
+  it("promise.then(success) 中的 success 会在 reslove 被调用的时候执行", done => {
+    const success = sinon.fake();
     const promise = new Promise((resolve, reject) => {
-      assert.isFalse(called);
+      assert.isFalse(success.called);
       resolve();
       setTimeout(() => {
-        assert.isTrue(called);
-        done();        
+        assert.isTrue(success.called);
+        done();
       });
     });
     // @ts-ignore
-    promise.then(() => {
-      called = true;
-    })
+    promise.then(success);
   });
-})
-
+});
