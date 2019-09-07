7610f065fe03ffbd3b315ab8ffa849de33446c3a
冬霖 <zhiyang_yang@126.com>
2019-09-07 16:38:10 +0800

    :tada: project init

---
+++ test/test.ts
@@ -0,0 +1,18 @@
+import * as chai from 'chai';
+
+const assert = chai.assert;
+
+describe('蔡徐坤', () => {
+  it('会唱', () => {
+    assert(1 === 1)
+  });
+  it('会跳', () => {
+    assert(2 === 2)
+  });
+  it('会rap', () => {
+    assert(3 === 3)
+  });
+  it('会篮球', () => {
+    assert(4 === 4)
+  })
+})
