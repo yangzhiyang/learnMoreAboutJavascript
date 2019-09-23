aedb9e7bdee863fab117c04efd46b93eee4968db
冬霖 <zhiyang_yang@126.com>
2019-09-23 22:41:08 +0800

    refactor: 优化代码

--- src/eventHub.ts
+++ src/eventHub.ts
@@ -1,35 +1,22 @@
 class Eventhub {
-  cache: { [key: string]: Array<(data: unknown) => void> } = {};
+  private cache: { [key: string]: Array<(data: unknown) => void> } = {};
+
   on(eventName: string, fn: (data?: unknown) => void) {
-    if (this.cache[eventName] === undefined) {
-      this.cache[eventName] = [];
-    }
+    this.cache[eventName] = this.cache[eventName] || [];
     this.cache[eventName].push(fn);
   }
+
   emit(eventName: string, data?: unknown) {
-    if (this.cache[eventName] === undefined) {
-      this.cache[eventName] = [];
-    }
+    this.cache[eventName] = this.cache[eventName] || [];
     this.cache[eventName].forEach(fn => {
       fn(data);
     });
   }
+
   off(eventName: string, fn: (data?: unknown) => void) {
-    if (this.cache[eventName] === undefined) {
-      this.cache[eventName] = [];
-    }
-    let index = undefined;
-    for (let i = 0; i < this.cache[eventName].length; i++) {
-      if (this.cache[eventName][i] === fn) {
-        index = i;
-        break;
-      }
-    }
-    if (index === undefined) {
-      return;
-    } else {
-      this.cache[eventName].splice(index, 1);
-    }
+    const index = this.cache[eventName].indexOf(fn);
+    if (index < 0) return;
+    this.cache[eventName].splice(index, 1);
   }
 }
 
