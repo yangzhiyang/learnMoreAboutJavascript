d10d40196b148dc7cfe140d5e4365e997bc9bff2
冬霖 <zhiyang_yang@126.com>
2019-09-23 22:14:44 +0800

    feat: 手写 eventHub

---
+++ src/eventHub.ts
@@ -0,0 +1,36 @@
+class Eventhub {
+  cache: { [key: string]: Array<(data: unknown) => void> } = {};
+  on(eventName: string, fn: (data?: unknown) => void) {
+    if (this.cache[eventName] === undefined) {
+      this.cache[eventName] = [];
+    }
+    this.cache[eventName].push(fn);
+  }
+  emit(eventName: string, data: unknown) {
+    if (this.cache[eventName] === undefined) {
+      this.cache[eventName] = [];
+    }
+    this.cache[eventName].forEach(fn => {
+      fn(data);
+    });
+  }
+  off(eventName: string, fn: (data: unknown) => void) {
+    if (this.cache[eventName] === undefined) {
+      this.cache[eventName] = [];
+    }
+    let index;
+    for (let i = 0; i < this.cache[eventName].length; i++) {
+      if (this.cache[eventName][i] === fn) {
+        index = i;
+        break;
+      }
+    }
+    if (!index) {
+      return;
+    } else {
+      this.cache[eventName].splice(index, 1);
+    }
+  }
+}
+
+export default Eventhub;
