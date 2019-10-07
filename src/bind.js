4f1613f946f8132bdf703d75ad46f6f66b1b88dd
冬霖 <zhiyang_yang@126.com>
2019-10-07 12:12:34 +0800

    feat: 完善 new 时对 prototype 的处理

--- src/bind.js
+++ src/bind.js
@@ -1,12 +1,15 @@
 function bind(asThis, ...args1) {
   const fn = this;
-  return function result(...args2) {
+
+  function result(...args2) {
     return fn.call(
       this.__proto__ === result.prototype ? this : asThis,
       ...args1,
       ...args2
     );
   };
+  result.prototype = fn.prototype;
+  return result;
 }
 
 module.exports = bind;
--- test/bind.ts
+++ test/bind.ts
@@ -33,17 +33,17 @@ describe("bind", () => {
     assert(newFn3(567)[0] === 123);
     assert(newFn3(567)[1] === 567);
   });
-  it("支持 new ", () => {
+  it("支持 new 时绑定参数，且有fn prototype 上的属性", () => {
     const fn = function(a1, a2) {
       this.a1 = a1;
       this.a2 = a2;
     };
-    // @ts-ignore
-    console.log(fn.__proto__, "this");
+    fn.prototype.test = "test";
     // @ts-ignore
     const newFn1 = fn.myBind({ name: "myBind" }, 123, 456);
     const newFn2 = new newFn1();
-    // assert(newFn2.a1 === 123);
-    // assert(newFn2.a2 === 456);
+    assert(newFn2.a1 === 123);
+    assert(newFn2.a2 === 456);
+    assert(newFn2.test === "test");
   });
 });
